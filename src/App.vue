5567fe5dc21462050e554771079fd0fc4e826f42
Burt <1020103647@qq.com>
2022-11-29 16:24:16 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>vite+vue2</h1>
+    <p>{{ count }}</p>
+    <button @click="add()">Add</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    add() {
+      this.count++;
+    },
+  },
+};
+</script>
+
+<style></style>
