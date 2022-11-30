fc1c97ccf050f4c5e625a568c1bf9bda510375b5
Burt <1020103647@qq.com>
2022-11-30 15:42:54 +0800

    feat: px转vw, 自动增加css前缀

--- src/views/HomeAbc.vue
+++ src/views/HomeAbc.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>Home Abc</div>
+  <div>
+    <div>Home Abc</div>
+    <div class="box">box</div>
+    <div class="flex">
+      <div class="item">item</div>
+      <div class="item">item</div>
+      <div class="item">item</div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,5 +16,13 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+.box {
+  height: 200px;
+  width: 375px;
+  background-color: aqua;
+}
+.flex {
+  display: flex;
+}
 </style>
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
 import { createVuePlugin } from "vite-plugin-vue2"
 import Components from "unplugin-vue-components/vite"
 import { VantResolver } from "unplugin-vue-components/resolvers"
+import pxtovw from "postcss-px-to-viewport"
+import autoprefixer from "autoprefixer"
+
+const init_pxtovw = pxtovw({
+  // 设计稿宽度
+  viewportWidth: 375,
+  viewportUnit: "vw",
+})
 
 export default {
   plugins: [
@@ -12,6 +20,24 @@ export default {
   resolve: {
     alias: {
       "@/": "/src/", //格式一定要写对喽不然没有代码提示或者报错
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [
+        init_pxtovw,
+        autoprefixer({
+          overrideBrowserslist: [
+            "Android 4.1",
+            "iOS 7.1",
+            "Chrome > 31",
+            "ff > 31",
+            "ie >= 8",
+            //'last 2 versions', // 所有主流浏览器最近2个版本
+          ],
+          grid: true,
+        }),
+      ],
     },
   },
 }
