2071f01990b4a9ed70c166a1dd078468475399f0
Burt <1020103647@qq.com>
2022-12-01 09:45:46 +0800

    feat: 可以自由使用ts

--- src/views/Setup.vue
+++ src/views/Setup.vue
@@ -5,20 +5,34 @@
     <p>可以支持类型！</p>
     <div>anther Count:{{count2}}</div>
     <van-button type="primary" @click="addOne2()">Add One</van-button>
+    <p>interface</p>
+    <p>User Name:{{user.name}}</p>
+    <p>User Age:{{user.age}}</p>
+    <Child />
+    <p>{{str}}</p>
   </div>
 
 </template>
 
-<script lang="ts">
-import { ref, Ref } from 'vue'
+<script lang="ts" name="SetupPage">
+import { ref, Ref, reactive } from 'vue'
+import { titleStr } from '@/utils/index'
+import Child from './Child.vue'
+interface User {
+  name: string
+  age: number
+}
 export default {
+  components: { Child },
   setup() {
     const count = ref(0)
     const count2: Ref<number> = ref(100)
     const addOne = () => count.value++
     const addOne2 = () => count2.value++
+    const user: User = reactive({ name: '张三', age: 10 })
+    const str = titleStr('abc')
     return {
-      count, count2, addOne, addOne2
+      count, count2, addOne, addOne2, user, Child, str
     }
   }
 }
